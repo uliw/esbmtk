1852cbfda41ba5b13a5fa3b9024deda6a5d7dcb7
uliw <uli.wortmann@utoronto.ca>
2021-05-20 14:25:37 -0400

    ReservoirGroups now have an optional seawaterconstants instance

--- esbmtk/utility_functions.py
+++ esbmtk/utility_functions.py
@@ -596,15 +596,16 @@ def create_reservoirs(bn: dict, ic: dict, M: any, cs: bool = False) -> dict:
                 concentration=icd[k][0],
                 isotopes=icd[k][1],
                 delta=icd[k][2],
+                seawater_parameters={"temperature": v["T"], " pressure": v["P"]},
             )
 
             if cs:
                 volume = Q_(f"{rg.lor[0].volume} l")
                 carbonate_system(
-                    Q_(f"{swc.ca} mol/l"),
-                    Q_(f"{swc.hplus} mol/l"),
+                    Q_(f"{rg.swc.ca} mol/l"),
+                    Q_(f"{rg.swc.hplus} mol/l"),
                     volume,
-                    swc,
+                    rg.swc,
                     rg,
                 )
 
