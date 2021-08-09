b2df5fc2e6cd2e365679d8b421eb53fca99a7ecf
uliw <uli.wortmann@utoronto.ca>
2021-08-09 13:02:58 -0400

    Changed to new naming scheme

--- tests/test_esbmtk.py
+++ tests/test_esbmtk.py
@@ -1,19 +1,20 @@
 def test_delta_conversion():
     from esbmtk import get_imass, get_delta, get_flux_data
 
-    r =0.0112372
+    r = 0.0112372
     m = 1
     d = 0
-    
-    l,h = get_imass(m, d, r)
-    d2 = get_delta(l,h,r)
+
+    l, h = get_imass(m, d, r)
+    d2 = get_delta(l, h, r)
     assert abs(d - d2) < 1e-10, "deltas should be similar"
-    A =  get_flux_data(m, d, r)
-    
+    A = get_flux_data(m, d, r)
+
     assert A[0] == m, "mass should be equal"
     assert A[1] == l, "light mass should be equal"
     assert A[2] == h, "heavy mass should be equal"
     assert A[3] == d, "deltas should be equal"
+
 
 def test_delta_conversion_with_vector():
     from esbmtk import get_imass, get_delta, get_flux_data
@@ -29,6 +30,7 @@ def test_delta_conversion_with_vector():
 
 def test_isotope_mass_calculation():
     from esbmtk import get_imass
+
     r = 0.0112372
 
     d = 21
@@ -38,6 +40,7 @@ def test_isotope_mass_calculation():
 
     assert m - li - hi == 0
 
+
 def test_fractionation_calculation():
     from esbmtk import get_imass, get_delta, get_frac
 
@@ -46,7 +49,7 @@ def test_fractionation_calculation():
     d = 21
     a = 1.003
 
-    l, h =  get_imass(m,d,r)
+    l, h = get_imass(m, d, r)
     l2, h2 = get_frac(m, l, a)
 
     a2 = (l * h2) / (h * l2)
@@ -54,11 +57,13 @@ def test_fractionation_calculation():
     diff = abs(a - a2)
     assert diff < 1e-15
 
+
 import pytest
+
 
 @pytest.fixture
 def create_model():
-    
+
     # from module import symbol
     from esbmtk import Model, Reservoir
 
@@ -66,52 +71,55 @@ def create_model():
     d0 = 0
     v0 = 1025
     # create model
-    Model(
+    M1 = Model(
         name="M1",  # model name
         stop="1 kyrs",  # end time of model
         timestep=" 1 yr",  # base unit for time
         mass_unit="mol",  # base unit for mass
         volume_unit="l",  # base unit for volume
         element="Carbon",  # load default element and species definitions
-        m_type = "both",
+        m_type="both",
     )
     Reservoir(
         name="R1",  # Name of reservoir
-        species=DIC,  # Species handle
+        species=M1.DIC,  # Species handle
         delta=d0,  # initial delta
-        concentration=f"{c0} mol/l",  # concentration 
+        concentration=f"{c0} mol/l",  # concentration
         volume=f"{v0} l",  # reservoir size (m^3)
     )
 
-    return v0, c0, d0, M1, R1,
+    return v0, c0, d0, M1
+
 
 def test_reservoir_creation(create_model):
-    """ Test that reservoir is initialized correctly
-    
-    """
-    v0, c0, d0, M1, R1 = create_model
+    """Test that reservoir is initialized correctly"""
+    v0, c0, d0, M1 = create_model
 
     mass = v0 * c0
-    assert R1.c[2] == c0
-    assert R1.m[2] == mass
-    assert abs(R1.d[2] - d0) < 1e-10
+    assert M1.R1.c[2] == c0
+    assert M1.R1.m[2] == mass
+    assert abs(M1.R1.d[2] - d0) < 1e-10
+
 
 def test_external_data(create_model):
-    """ test the creation of an external data object
-    
-    """
+    """test the creation of an external data object"""
 
     from esbmtk import ExternalData
-    ExternalData(name="ED1",
-                 filename = "measured_c_isotopes.csv",
-                 legend="ED1",
-                 reservoir=R1,
-                 offset="0.1 kyrs",
-                 )
-    assert ED1.x[0] == 100
+
+    v0, c0, d0, M1 = create_model
+
+    ExternalData(
+        name="ED1",
+        filename="measured_c_isotopes.csv",
+        legend="ED1",
+        reservoir=M1.R1,
+        offset="0.1 kyrs",
+    )
+    assert M1.ED1.x[0] == 100
     assert "Unnamed" in ED1.df.columns[1]
-    assert round(ED1.z[0],10) == 2.09512
-    assert round(ED1.z[-1],10) == 0.968293
+    assert round(ED1.z[0], 10) == 2.09512
+    assert round(ED1.z[-1], 10) == 0.968293
+
 
 def test_sum_fluxes(create_model):
     """
@@ -120,47 +128,47 @@ def test_sum_fluxes(create_model):
 
     from esbmtk import Connect, Source, Sink
 
-    v0, c0, d0, M1, R1 = create_model
-    
-    Source(name="SO1", species=CO2)
-    Source(name="SO2", species=CO2)
-    Sink(name="SI1", species=CO2)
-    Sink(name="SI2", species=CO2)
+    v0, c0, d0, M1 = create_model
+
+    Source(name="SO1", species=M1.CO2)
+    Source(name="SO2", species=M1.CO2)
+    Sink(name="SI1", species=M1.CO2)
+    Sink(name="SI2", species=M1.CO2)
 
     Connect(
-        source=SO1,  # source of flux
-        sink=R1,  # target of flux
-        rate="100 mol/yr",  # weathering flux in 
+        source=M1.SO1,  # source of flux
+        sink=M1.R1,  # target of flux
+        rate="100 mol/yr",  # weathering flux in
         delta=d0,  # set a default flux
     )
     Connect(
-        source=SO2,  # source of flux
-        sink=R1,  # target of flux
-        rate="300 mol/yr",  # weathering flux in 
-        delta=d0,  # set a default flux
-    )
-
-    Connect(
-        source=R1,  # source of flux
-        sink=SI1,  # target of flux
-        rate="250 mol/yr",  # weathering flux in 
+        source=M1.SO2,  # source of flux
+        sink=M1.R1,  # target of flux
+        rate="300 mol/yr",  # weathering flux in
         delta=d0,  # set a default flux
     )
 
     Connect(
-        source=R1,  # source of flux
-        sink=SI2,  # target of flux
-        rate="150 mol/yr",  # weathering flux in 
+        source=M1.R1,  # source of flux
+        sink=M1.SI1,  # target of flux
+        rate="250 mol/yr",  # weathering flux in
         delta=d0,  # set a default flux
     )
 
-   
+    Connect(
+        source=M1.R1,  # source of flux
+        sink=M1.SI2,  # target of flux
+        rate="150 mol/yr",  # weathering flux in
+        delta=d0,  # set a default flux
+    )
+
     M1.run()
-    assert R1.c[-2] == c0
-    R1.d[-2] == d0
+    assert M1.R1.c[-2] == c0
+    M1.R1.d[-2] == d0
 
     # strangely, this fails at the moment
     # assert R1.d[-]2 == 1
+
 
 def test_passive_sum(create_model):
     """
@@ -169,38 +177,38 @@ def test_passive_sum(create_model):
 
     from esbmtk import Connect, Source, Sink
 
-    v0, c0, d0, M1, R1 = create_model
+    v0, c0, d0, M1 = create_model
 
-    Source(name="SO1", species=CO2)
-    Source(name="SO2", species=CO2)
-    Sink(name="SI1", species=CO2)
-    Sink(name="SI2", species=CO2)
+    Source(name="SO1", species=M1.CO2)
+    Source(name="SO2", species=M1.CO2)
+    Sink(name="SI1", species=M1.CO2)
+    Sink(name="SI2", species=M1.CO2)
 
     Connect(
-        source=SO1,  # source of flux
-        sink=R1,  # target of flux
-        rate="100 mol/yr",  # weathering flux in 
+        source=M1.SO1,  # source of flux
+        sink=M1.R1,  # target of flux
+        rate="100 mol/yr",  # weathering flux in
         delta=d0,  # set a default flux
     )
     Connect(
-        source=SO2,  # source of flux
-        sink=R1,  # target of flux
-        rate="300 mol/yr",  # weathering flux in 
+        source=M1SO2,  # source of flux
+        sink=M1.R1,  # target of flux
+        rate="300 mol/yr",  # weathering flux in
         delta=d0,  # set a default flux
     )
 
     Connect(
-        source=R1,  # source of flux
-        sink=SI1,  # target of flux
-        rate="250 mol/yr",  # weathering flux in 
+        source=M1.R1,  # source of flux
+        sink=M1.SI1,  # target of flux
+        rate="250 mol/yr",  # weathering flux in
         delta=0,  # set a default flux
     )
 
     Connect(
-        source=R1,  # source of flux
-        sink=SI2,  # target of flux
+        source=M1.R1,  # source of flux
+        sink=M1.SI2,  # target of flux
     )
 
     M1.run()
-    assert R1.c[-2] == c0
-    assert abs(R1.d[-2] - d0) < 1e-10
+    assert M1.R1.c[-2] == c0
+    assert abs(M1.R1.d[-2] - d0) < 1e-10
