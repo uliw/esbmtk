b277b74fbb605fc8a050c892f84969f363883c08
uliw <uli.wortmann@utoronto.ca>
2022-04-09 12:56:59 -0400

    added parent handles so that spcies register with model

--- esbmtk/species_definitions.py
+++ esbmtk/species_definitions.py
@@ -32,24 +32,27 @@ def carbon(model):
         d_label=r"$\delta^{13}$C",  # Name of isotope delta
         d_scale="VPDB",  # Isotope scale. End of plot labels
         r=0.0112372,  # VPDB C13/C12 ratio https://www-pub.iaea.org/MTCD/publications/PDF/te_825_prn.pdf
+        register=model,
     )
 
     # add species
-    Species(name="CO2", element=eh, display_as=r"CO$_2$")  # Name & element handle
-    Species(name="DIC", element=eh)
-    Species(name="OM", element=eh)
-    Species(name="CaCO3", element=eh, display_as=r"CaCO$_3$")
-    Species(name="HCO3", element=eh, display_as=r"HCO$_3^-$")
-    Species(name="CO3", element=eh, display_as="CO$_3^{2-}$")
-    Species(name="DOC", element=eh)
-    Species(name="C", element=eh)
-    Species(name="ALK", element=eh)  # Alkalinity
-    Species(name="CALK", element=eh)  # Carbonate Alkalinity
-    Species(name="CA", element=eh)
-    Species(name="TALK", element=eh)  # Total Alkalinity
-    Species(name="TA", element=eh)
-    Species(name="PALK", element=eh)  # Practical Alkalinity
-    Species(name="PA", element=eh)
+    Species(
+        name="CO2", element=eh, display_as=r"CO$_2$", register=eh
+    )  # Name & element handle
+    Species(name="DIC", element=eh, register=eh)
+    Species(name="OM", element=eh, register=eh)
+    Species(name="CaCO3", element=eh, display_as=r"CaCO$_3$", register=eh)
+    Species(name="HCO3", element=eh, display_as=r"HCO$_3^-$", register=eh)
+    Species(name="CO3", element=eh, display_as="CO$_3^{2-}$", register=eh)
+    Species(name="DOC", element=eh, register=eh)
+    Species(name="C", element=eh, register=eh)
+    Species(name="ALK", element=eh, register=eh)  # Alkalinity
+    Species(name="CALK", element=eh, register=eh)  # Carbonate Alkalinity
+    Species(name="CA", element=eh, register=eh)
+    Species(name="TALK", element=eh, register=eh)  # Total Alkalinity
+    Species(name="TA", element=eh, register=eh)
+    Species(name="PALK", element=eh, register=eh)  # Practical Alkalinity
+    Species(name="PA", element=eh, register=eh)
 
 
 def sulfur(model):
@@ -62,20 +65,21 @@ def sulfur(model):
         d_label="$\delta^{34}$S",  # Name of isotope delta
         d_scale="VCDT",  # Isotope scale. End of plot labels
         r=0.044162589,  # isotopic abundance ratio for species
+        register=model,
     )
 
     # add species
-    Species(name="SO4", element=eh, display_as=r"SO$_{4}^{2-}$")
-    Species(name="SO4oxygen", element=eh, display_as=r"SO$_{4}^{2-}{_ox}$")
-    Species(name="SO3", element=eh, display_as=r"SO$_{3}$")
-    Species(name="SO2", element=eh, display_as=r"SO$_{2$}")
-    Species(name="HS", element=eh, display_as=r"HS$^-$")
-    Species(name="H2S", element=eh, display_as=r"H$_{2}$S")
-    Species(name="FeS", element=eh)
-    Species(name="FeS2", element=eh, display_as=r"FeS$_{2}$")
-    Species(name="S0", element=eh)
-    Species(name="S", element=eh)
-    Species(name="S2minus", element=eh, display_as=r"S$^{2-}$")
+    Species(name="SO4", element=eh, display_as=r"SO$_{4}^{2-}$", register=eh)
+    Species(name="SO4oxygen", element=eh, display_as=r"SO$_{4}^{2-}{_ox}$", register=eh)
+    Species(name="SO3", element=eh, display_as=r"SO$_{3}$", register=eh)
+    Species(name="SO2", element=eh, display_as=r"SO$_{2$}", register=eh)
+    Species(name="HS", element=eh, display_as=r"HS$^-$", register=eh)
+    Species(name="H2S", element=eh, display_as=r"H$_{2}$S", register=eh)
+    Species(name="FeS", element=eh, register=eh)
+    Species(name="FeS2", element=eh, display_as=r"FeS$_{2}$", register=eh)
+    Species(name="S0", element=eh, register=eh)
+    Species(name="S", element=eh, register=eh)
+    Species(name="S2minus", element=eh, display_as=r"S$^{2-}$", register=eh)
 
 
 def hydrogen(model):
@@ -88,12 +92,17 @@ def hydrogen(model):
         d_label=r"$\delta^{2}$D",  # Name of isotope delta
         d_scale="VSMOV",  # Isotope scale. End of plot labels  # needs verification
         r=155.601e-6,  # After Groenig, 2000, Tab 40.1
+        register=model,
     )
 
     # add species
-    Species(name="Hplus", element=eh, display_as=r"$H^+$")  # Name & element handle
-    Species(name="H2O", element=eh, display_as=r"H$_{2}$O")  # Name & element handle
-    Species(name="H", element=eh)  # Name & element handle
+    Species(
+        name="Hplus", element=eh, display_as=r"$H^+$", register=eh
+    )  # Name & element handle
+    Species(
+        name="H2O", element=eh, display_as=r"H$_{2}$O", register=eh
+    )  # Name & element handle
+    Species(name="H", element=eh, register=eh)  # Name & element handle
 
 
 def oxygen(model):
@@ -106,13 +115,18 @@ def oxygen(model):
         d_label=r"$\delta^{18}$)",  # Name of isotope delta
         d_scale="VSMOV",  # Isotope scale. End of plot labels  # needs verification
         r=2005.201e-6,  # https://nucleus.iaea.org/rpst/documents/vsmow_slap.pdf
+        register=model,
     )
 
     # add species
-    Species(name="O", element=eh, display_as="O")
+    Species(name="O", element=eh, display_as="O", register=eh)
     # Name & element handle
-    Species(name="O2", element=eh, display_as=r"O$_{2}$")  # Name & element handle
-    Species(name="OH", element=eh, display_as=r"OH$^{-}$")  # Name & element handle
+    Species(
+        name="O2", element=eh, display_as=r"O$_{2}$", register=eh
+    )  # Name & element handle
+    Species(
+        name="OH", element=eh, display_as=r"OH$^{-}$", register=eh
+    )  # Name & element handle
 
 
 def phosphor(model):
@@ -125,11 +139,14 @@ def phosphor(model):
         d_label="D",  # Name of isotope delta
         d_scale="DS",  # Isotope scale. End of plot labels  # needs verification
         r=1,  # isotopic abundance ratio for species # needs verification
+        register=model,
     )
 
     # add species
-    Species(name="PO4", element=eh, display_as=r"PO$_{4}$")  # Name & element handle
-    Species(name="P", element=eh, display_as=r"P")  # Name & element handle
+    Species(
+        name="PO4", element=eh, display_as=r"PO$_{4}$", register=eh
+    )  # Name & element handle
+    Species(name="P", element=eh, display_as=r"P", register=eh)  # Name & element handle
 
 
 def nitrogen(model):
@@ -142,14 +159,23 @@ def nitrogen(model):
         d_label=r"$\delta^{15}$N",  # Name of isotope delta
         d_scale="Air",  # Isotope scale. End of plot labels  # needs verification
         r=3676.5e-6,  # isotopic abundance ratio for species # needs verification
+        register=model,
     )
 
     # add species
-    Species(name="N", element=eh, display_as=r"N")
-    Species(name="N2", element=eh, display_as=r"N$_{2}$")  # Name & element handle
-    Species(name="Nox", element=eh, display_as=r"Nox")  # Name & element handle
-    Species(name="NH4", element=eh, display_as=r"NH$_{4}^{+}$")  # Name & element handle
-    Species(name="NH3", element=eh, display_as=r"NH$_{3}$")  # Name & element handle
+    Species(name="N", element=eh, display_as=r"N", register=eh)
+    Species(
+        name="N2", element=eh, display_as=r"N$_{2}$", register=eh
+    )  # Name & element handle
+    Species(
+        name="Nox", element=eh, display_as=r"Nox", register=eh
+    )  # Name & element handle
+    Species(
+        name="NH4", element=eh, display_as=r"NH$_{4}^{+}$", register=eh
+    )  # Name & element handle
+    Species(
+        name="NH3", element=eh, display_as=r"NH$_{3}$", register=eh
+    )  # Name & element handle
 
 
 def boron(model):
@@ -162,9 +188,14 @@ def boron(model):
         d_label=r"$\delta{11}B",  # Name of isotope delta
         d_scale="SRM951",  # Isotope scale. End of plot labels  # needs verification
         r=0.26888,  # isotopic abundance ratio for species # needs verification
+        register=model,
     )
 
     # add species
-    Species(name="B", element=eh, display_as=r"B")  # Name & element handle
-    Species(name="BOH3", element=eh, display_as=r"B(OH)$_{3}$")  # Boric Acid
-    Species(name="BOH4", element=eh, display_as=r"B(OH)$_{4}^{-}$")  # Borate
+    Species(name="B", element=eh, display_as=r"B", register=eh)  # Name & element handle
+    Species(
+        name="BOH3", element=eh, display_as=r"B(OH)$_{3}$", register=eh
+    )  # Boric Acid
+    Species(
+        name="BOH4", element=eh, display_as=r"B(OH)$_{4}^{-}$", register=eh
+    )  # Borate
