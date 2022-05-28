057e0ccfc45deb72a92a7e259b4ee7e95249ecf6
rubentium <97776374+rubentium@users.noreply.github.com>
2022-05-28 15:10:03 -0400

    Update ODEINT_Solver.py
    Added the license and elaborated on docstrings

--- esbmtk/ODEINT_Solver.py
+++ esbmtk/ODEINT_Solver.py
@@ -1,3 +1,20 @@
+"""
+ esbmtk: A general purpose Earth Science box model toolkit
+ Copyright (C), 2020 Ulrich G. Wortmann
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from __future__ import annotations
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
@@ -9,10 +26,28 @@ if tp.TYPE_CHECKING:
 
 
 class Constructor:
-    """constructs the equation from the terms
-    in res_flux dictionary"""
+    """This class is for constructing the equations
+    when the EQ_Terms object is inputted together
+    with the res_flux dictionary (which is an attribute).
+    
+    It constructs the equations by looping over the tuples
+    in the res_flux dictionary and for every tuple,
+    it summs up the elements of the first list and sums the
+    elementrs of the second list and subtracts the second
+    sum from the first and divides the result by the volume
+    of the reservoir for whovh the equation is constructed.
+    Thus getting a sympy evaluable expression of the form
+    
+    concentration = 
+    (inflowing-terms - outflowing-terms)/volume-of-reservoir
+    
+    and appends a list to include these expressions in order
+    """
 
     def __init__(self, terms: EQ_Terms):
+        """The entire aforementioned process
+        is carried during the initialization of
+        the Constructor object"""
         self.terms = terms
         # tuple of resevoir ids (also res_flux keys)
         self.var_ids = tuple(terms.res_flux.keys())
@@ -38,12 +73,25 @@ class Constructor:
 
 
 class run_solver:
-    """runs the odeint solver on the equations
-    and if want_a_plot==True, it also plots the graph"""
+    """In order to generate the equations, solve the
+    system, and plot it the wanted Model M is inputted
+    for initialization.
+    
+     It utelizes the EQ_Terms and Constructor classes
+     to first generate the terms of the equations and store
+     them in res_flux and later inputs this EQ_Terms object into
+     Constructor to generate the system itself.
+     
+     The class uses scipy.integrate.odeint package
+     to solve the system of ODEs.
+     
+     If want_a_plot = True during initialization,
+     then the plot will be provided but the default
+     if False."""
 
     def __init__(self, M, want_a_plot: bool = False):
-        """activates the solver and the plotting method
-        M is a Model class object n esbmtk"""
+        """Activates the solver and the plotting methods
+        M is a Model class object in esbmtk"""
         from esbmtk import EQ_Terms
 
         eq_terms = EQ_Terms(M)
@@ -53,7 +101,10 @@ class run_solver:
             self.plot(construct)
 
     def _solve(self, K: Constructor):
-        """solves the ODE system"""
+        """Solves the ODE system.
+        The K is the Constructor object
+        from __init__
+        """
 
         from esbmtk import Q_
 
@@ -61,7 +112,8 @@ class run_solver:
         vars = K.vars
 
         def kin(z, t):
-            """the ode system function"""
+            """The ode system function.
+            Computes the derivative of z at t."""
 
             dzdt = []
             dics = {}
@@ -84,7 +136,9 @@ class run_solver:
             K.terms.res_flux[res_id][3].c = self.sol[:, var_index]
 
     def plot(self, K: Constructor):
-        """plots the ODE system"""
+        """Plots the output of the _solve method
+        if want_a_plot = True. It plots all of the
+        plots on one graph."""
 
         from esbmtk import Q_
 
