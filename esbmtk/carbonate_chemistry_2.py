bf871557bd14c2e1c9368863b0861beb890afd87
uliw <uli.wortmann@utoronto.ca>
2022-06-17 15:50:01 -0400

    various changes to cs2

--- esbmtk/carbonate_chemistry_2.py
+++ esbmtk/carbonate_chemistry_2.py
@@ -74,83 +74,25 @@ def carbonate_system_1_ode(rg: ReservoirGroup, dic: float, ta: float, i: int) ->
     #  co2aq: float = dic / (1 + (k1 / hplus) + (k1 * k2 / (hplus ** 2)))
     co2aq: float = dic - hco3 - co3
 
-    # print(f"i = {i}, t = {t} pH({i}) = {-np.log10(hplus):.2f}, pH({i-1}) = {-np.log10(rg.cs.H[i-1]):.2f}")
-
     rg.cs.H[0] = hplus  #
-    rg.cs.CA[0] = ca
-    rg.cs.HCO3[0] = hco3
-    rg.cs.CO3[0] = co3
-    rg.cs.CO2aq[0] = co2aq
+    # rg.cs.CA[0] = ca
+    # rg.cs.HCO3[0] = hco3
+    # rg.cs.CO3[0] = co3
+    # rg.cs.CO2aq[0] = co2aq
 
     return co2aq
 
 
-def carbonate_system_1_ode_old(
-    rg: ReservoirGroup, dic: float, ta: float, i: int, t
-) -> tuple:
-    """Calculates and returns the carbonate concentrations and saturation state
-     for the given reservoirgroup
-
-    LIMITATIONS:
-    - Assumes all concentrations are in mol/kg
-    - Assumes your Model is in mol/kg ! Otherwise, DIC and TA updating will not
-    be correct.
-
-    Calculations are based off equations from:
-    Boudreau et al., 2010, https://doi.org/10.1029/2009GB003654
-    Follows, 2006, doi:10.1016/j.ocemod.2005.05.004
-
-    Author: M. Niazi & T. Tsan, 2021, with modifications by U. Wortmann 2022
-    """
-
-    k1 = rg.swc.K1  # K1
-    k2 = rg.swc.K2  # K2
-    KW = rg.swc.KW  # KW
-    KB = rg.swc.KB  # KB
-    boron = rg.swc.boron  # boron
-    hplus = rg.cs.H[i - 1]  # hplus from previous time step
-
-    # calculates carbonate alkalinity (ca) based on H+ concentration from the
-    # previous time-step
-    oh: float = KW / hplus
-    boh4: float = boron * KB / (hplus + KB)
-    fg: float = hplus - oh - boh4
-    ca: float = ta + fg
-
-    # hplus
-    gamm: float = dic / ca
-    dummy: float = (1 - gamm) * (1 - gamm) * k1 * k1 - 4 * k1 * k2 * (1 - (2 * gamm))
-
-    hplus: float = 0.5 * ((gamm - 1) * k1 + (dummy**0.5))
-    # hco3 and co3
-    """ Since CA = [hco3] + 2[co3], can the below expression can be simplified
-    """
-    # co3: float = dic / (1 + (hplus / k2) + ((hplus ** 2) / (k1 * k2)))
-    hco3: float = dic / (1 + (hplus / k1) + (k2 / hplus))
-    co3: float = (ca - hco3) / 2
-    # co2 (aq)
-    """DIC = hco3 + co3 + co2 + H2CO3 The last term is however rather
-    small, so it may be ok to simply write co2aq = dic - hco3 + co3.
-    Let's test this once we have a case where pco2 is calculated from co2aq
-    """
-    #  co2aq: float = dic / (1 + (k1 / hplus) + (k1 * k2 / (hplus ** 2)))
-    co2aq: float = dic - hco3 - co3
-
-    print(
-        f"i = {i}, t = {t} pH({i}) = {-np.log10(hplus):.2f}, pH({i-1}) = {-np.log10(rg.cs.H[i-1]):.2f}"
-    )
-
-    rg.cs.H[i] = hplus
-    rg.cs.CA[i] = ca
-    rg.cs.HCO3[i] = hco3
-    rg.cs.CO3[i] = co3
-    rg.cs.CO2aq[i] = co2aq
-
-
 def carbonate_system_2_ode(
-    t, rg: ReservoirGroup, Bm: Flux, dic: float, ta: float
+    t,
+    rg: ReservoirGroup,
+    Bm: Flux,
+    dic: float,
+    ta: float,
+    i: float,
+    last_t: float,
 ) -> float:
-    """Calculates and returns the carbonate concentrations and carbonate compensation
+    """Calculates and returns the carbonate burial flux and carbonate compensation
     depth (zcc) at the ith time-step of the model.
 
 
@@ -166,30 +108,29 @@ def carbonate_system_2_ode(
 
     Author: M. Niazi & T. Tsan, 2021
     """
+    
+    # concentration
+    hplus = rg.cs.H[0]  # hplus from last timestep
 
     # Parameters
-    k1 = rg.swc.K1  # K1
-    k2 = rg.swc.K2  # K2
-    KW = rg.swc.KW  # KW
-    KB = rg.swc.KB  # KB
-    ksp0 = rg.swc.Ksp0
+    p =  rg.cs.function_params
+    
+    k1 = p[0]  # K1
+    k2 = p[1]  # K2
+    KW = p[2]  # KW
+    KB = p[3]  # KB
+    boron = p[4]  # boron
+    ksp0 = p[5]
     ca2 = rg.swc.ca2  # Ca2+
-    boron = rg.swc.boron  # boron
-
-    # concentration
-    hplus = rg.cs.H  # hplus from last timestep
-
-    # still missing parameters
-    last_t = rg.cs.last_t
-    kc = rg.cs.kc
-    AD = rg.cs.AD
-    zsat0 = rg.cs.zsat0
-    I_caco3 = rg.cs.I_caco3
-    alpha = rg.cs.alpha
-    zsat_min = int(abs(rg.cs.zsat_min))
-    zmax = int(abs(rg.cs.zmax))
-    z0 = int(abs(rg.cs.z0))
-    zsnow = rg.cs.zsnow  # previous zsnow
+    kc = p[6]
+    AD = p[8]
+    zsat0 = p[9]
+    I_caco3 = p[12]
+    alpha = p[13]
+    zsat_min = int(abs(p[14]))
+    zmax = int(abs(p[15]))
+    z0 = int(abs(p[16]))
+    zsnow = int(abs(p[19]))  # previous zsnow
     depth_area_table = rg.cs.depth_area_table
     area_dz_table = rg.cs.area_dz_table
     Csat_table = rg.cs.Csat_table
@@ -278,15 +219,14 @@ def carbonate_system_2_ode(
     # diss12 = (B12 - Fburial12) * dt  # dissolution flux light isotope
 
     # copy results into datafields
-    rg.cs.H = hplus  # 0
-    rg.cs.CA = ca  # 1
-    rg.cs.HCO3 = hco3  # 2
-    rg.cs.CO3 = co3  # 3
-    rg.cs.CO2aq = co2aq  # 4
-    rg.cs.zsat = zsat  # 5
-    rg.cs.zcc = zcc  # 6
-    rg.cs.zsnow = zsnow  # 7
-    rg.cs.last_t = t
+    rg.cs.H[0] = hplus  # 0
+    rg.cs.CA[i] = ca  # 1
+    rg.cs.HCO3[i] = hco3  # 2
+    rg.cs.CO3[i] = co3  # 3
+    rg.cs.CO2aq[i] = co2aq  # 4
+    rg.cs.zsat[i] = zsat  # 5
+    rg.cs.zcc[i] = zcc  # 6
+    rg.cs.zsnow[i] = zsnow  # 7
 
     return Fburial
 
