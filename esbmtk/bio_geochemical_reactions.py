b03927d68961c4fcc6cb361548de80efc0cfda24
uliw <uli.wortmann@utoronto.ca>
2023-08-17 08:24:52 +0200

    add cs3

--- esbmtk/bio_geochemical_reactions.py
+++ esbmtk/bio_geochemical_reactions.py
@@ -18,7 +18,10 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
 import typing as tp
+import numpy as np
+from math import log, sqrt
 from esbmtk import Q_, register_return_values
+from .utility_functions import __checkkeys__, __addmissingdefaults__, __checktypes__
 
 if tp.TYPE_CHECKING:
     from esbmtk import Flux, Model, ReservoirGroup
@@ -100,7 +103,7 @@ def init_photosynthesis(rg, productivity):
         species=rg.mo.Oxygen.O2,
         fname="photosynthesis",
         ftype="cs2",  # cs1 is independent of fluxes, cs2 is not
-        vr_datafields={"OM": 0, "OM_l": 0},
+        vr_datafields={"OM": 0.0, "OM_l": 0.0},
         function_input_data=[
             rg.O2,
             rg.TA,
@@ -115,7 +118,7 @@ def init_photosynthesis(rg, productivity):
             M.O2C_ratio,
             M.PUE,
             M.rain_rate,
-            M.OM_frac / 1000 + 1,  # convert to actual ratio
+            M.OM_frac / 1000.0 + 1.0,  # convert to actual ratio
             M.C.r,  # VDPB reference ratio
         ],
         register=rg,
@@ -187,11 +190,11 @@ def remineralization(
     dMdt_dic = OM_flux  # add DIC from OM
     dMdt_dic_l = OM_flux_l
 
-    print(f"OM_flux = {OM_flux:2e}")
-    print(f"dMdt_po4 = {dMdt_po4:2e}")
-    print(f"dMdt_ta = {dMdt_ta:2e}")
-    print(f"dMdt_dic = {dMdt_dic:2e}")
-    print(f"dMdt_dic_l = {dMdt_dic_l:2e}")
+    # print(f"OM_flux = {OM_flux:2e}")
+    # print(f"dMdt_po4 = {dMdt_po4:2e}")
+    # print(f"dMdt_ta = {dMdt_ta:2e}")
+    # print(f"dMdt_dic = {dMdt_dic:2e}")
+    # print(f"dMdt_dic_l = {dMdt_dic_l:2e}")
 
     m_h2s = h2s * volume
     m_o2 = o2 * volume
@@ -202,10 +205,10 @@ def remineralization(
         dMdt_o2 = -m_o2_eq  # consume O2
         dMdt_h2s = -m_h2s  # consume all h2s
         dMdt_so4 = -m_h2s  # add sulfate
-        print("oxic remin")
-        print(f"dMdt_o2 = {dMdt_o2:2e}")
-        print(f"dMdt_h2s = {dMdt_h2s:2e}")
-        print(f"dMdt_so4 = {dMdt_so4:2e}")
+        # print("oxic remin")
+        # print(f"dMdt_o2 = {dMdt_o2:2e}")
+        # print(f"dMdt_h2s = {dMdt_h2s:2e}")
+        # print(f"dMdt_so4 = {dMdt_so4:2e}")
 
     else:  # box has not enough oxygen
         dMdt_o2 = -m_o2  # remove all available oxygen
@@ -215,24 +218,23 @@ def remineralization(
         dMdt_so4 = -OM_flux / 2  # one SO4 oxidizes 2 carbon, and add 2 mol to TA
         dMdt_h2s = -dMdt_so4  # move S to reduced reservoir
         dMdt_ta += 2 * -dMdt_so4  # adjust Alkalinity for changes in sulfate
-        print("anoxic remin")
-        print(f"dMdt_o2 = {dMdt_o2:2e}")
-        print(f"dMdt_h2s = {dMdt_h2s:2e}")
-        print(f"dMdt_so4 = {dMdt_so4:2e}")
-        print(f"dMdt_ta = {dMdt_ta:2e}")
+        # print("anoxic remin")
+        # print(f"dMdt_o2 = {dMdt_o2:2e}")
+        # print(f"dMdt_h2s = {dMdt_h2s:2e}")
+        # print(f"dMdt_so4 = {dMdt_so4:2e}")
+        # print(f"dMdt_ta = {dMdt_ta:2e}")
 
     if CaCO3_reactions:
-        dic_flux = 0
-        dic_flux_l = 0
+        dic_flux = 0.0
+        dic_flux_l = 0.0
         for i, f in enumerate(dic_fluxes):
             dic_flux += f * alpha[i]
             dic_flux_l += dic_fluxes_l[i] * alpha[i]
 
-        breakpoint()
         # add Alkalinity and DIC from CaCO3 dissolution
-        dMdt_dic += dic_flux
-        dMdt_dic_l += dic_flux_l
-        dMdt_ta += 2 * dic_flux
+        dMdt_dic += -dic_flux
+        dMdt_dic_l += -dic_flux_l
+        dMdt_ta += 2 * -dic_flux
 
     # note, these are returned as fluxes
     return [dMdt_ta, dMdt_h2s, dMdt_so4, dMdt_o2, dMdt_po4]
@@ -324,6 +326,7 @@ def add_remineralization(M: Model, f_map: dict) -> None:
                     return_list=True,
                 )
                 for f in fl:
+                    print(f" = {f}")
                     om_remin.append(type_dict["OM"])
                     if f.name[-3:] == "F_l":
                         om_fluxes_l.append(f)
@@ -342,8 +345,8 @@ def add_remineralization(M: Model, f_map: dict) -> None:
                     else:
                         CaCO3_fluxes.append(f)
 
-        #print(f"CaCO3 fluxes {sink.full_name} {CaCO3_fluxes}")
-        #print(f"OM fluxes {sink.full_name} {om_fluxes}")
+        # print(f"CaCO3 fluxes {sink.full_name} {CaCO3_fluxes}")
+        # print(f"OM fluxes {sink.full_name} {om_fluxes}")
         if len(CaCO3_fluxes) > 0:
             ec = init_remineralization(
                 sink,
@@ -368,3 +371,301 @@ def add_remineralization(M: Model, f_map: dict) -> None:
             )
         register_return_values(ec, sink)
         sink.has_cs2 = True
+
+
+def carbonate_system_3(
+    rg: ReservoirGroup,  # 2 Reservoir handle
+    CaCO3_export: float,  # 3 CaCO3 export flux as DIC
+    dic_db: float,  # 4 DIC in the deep box
+    dic_db_l: float,  # 4 DIC in the deep box
+    ta_db: float,  # 5 TA in the deep box
+    dic_sb: float,  # 6 [DIC] in the surface box
+    dic_sb_l: float,  # 7 [DIC_l] in the surface box
+    hplus_0: float,  # 8 hplus in the deep box at t-1
+    zsnow: float,  # 9 snowline in meters below sealevel at t-1
+    ksp0,
+    kc,
+    AD,
+    zsat0,
+    I_caco3,
+    alpha,
+    zsat_min,
+    zmax,
+    z0,
+) -> tuple:
+    """Calculates and returns the fraction of the carbonate rain that is
+    dissolved an returned back into the ocean. This functions returns:
+
+    DIC_burial, DIC_burial_l, Hplus, zsnow
+
+    LIMITATIONS:
+    - Assumes all concentrations are in mol/kg
+    - Assumes your Model is in mol/kg ! Otherwise, DIC and TA updating will not
+    be correct.
+
+    Calculations are based off equations from:
+    Boudreau et al., 2010, https://doi.org/10.1029/2009GB003654
+    Follows, 2006, doi:10.1016/j.ocemod.2005.05.004
+    """
+
+    # Parameters
+    k1 = rg.swc.K1  # K1
+    k1k1 = rg.swc.K1K1
+    k2 = rg.swc.K2  # K2
+    k1k2 = rg.swc.K1K2  # K2
+    KW = rg.swc.KW  # KW
+    KB = rg.swc.KB  # KB
+    ca2 = rg.swc.ca2  # Ca2+
+    boron = rg.swc.boron  # boron
+    zsat0 = int(abs(zsat0))
+    zsat_min = int(abs(zsat_min))
+    zmax = int(abs(zmax))
+    z0 = int(abs(z0))
+    depth_area_table = rg.cs.depth_area_table
+    area_dz_table = rg.cs.area_dz_table
+    Csat_table = rg.cs.Csat_table
+
+    # calc carbonate alkalinity based t-1
+    oh: float = KW / hplus_0
+    boh4: float = boron * KB / (hplus_0 + KB)
+    fg: float = hplus_0 - oh - boh4
+    ca: float = ta_db + fg
+
+    # calculate carbon speciation
+    # The following equations are after Follows et al. 2006
+    gamm: float = dic_db / ca
+    dummy: float = (1 - gamm) * (1 - gamm) * k1k1 - k1k2 * (4 - 8 * gamm)
+    hplus: float = 0.5 * ((gamm - 1) * k1 + sqrt(dummy))
+    co3 = max(dic_db / (1 + hplus / k2 + hplus * hplus / k1k2), 3.7e-05)
+    # ---------- compute critical depth intervals eq after  Boudreau (2010)
+    # all depths will be positive to facilitate the use of lookup_tables
+    zsat = int(zsat0 * log(ca2 * co3 / ksp0))
+    zsat = np.clip(zsat, zsat_min, zmax)
+    zcc = int(
+        zsat0 * log(-CaCO3_export * ca2 / (ksp0 * AD * kc) + ca2 * co3 / ksp0)
+    )  # eq3
+    zcc = np.clip(zcc, zsat_min, zmax)
+    # get fractional areas
+    B_AD = -CaCO3_export / AD
+    A_z0_zsat = depth_area_table[z0] - depth_area_table[zsat]
+    A_zsat_zcc = depth_area_table[zsat] - depth_area_table[zcc]
+    A_zcc_zmax = depth_area_table[zcc] - depth_area_table[zmax]
+    # ------------------------Calculate Burial Fluxes----------------------------- #
+    BCC = A_zcc_zmax * B_AD
+    BNS = alpha * A_z0_zsat * B_AD
+    diff_co3 = Csat_table[zsat:zcc] - co3
+    area_p = area_dz_table[zsat:zcc]
+    BDS_under = kc * area_p.dot(diff_co3)
+    BDS_resp = alpha * (A_zsat_zcc * B_AD - BDS_under)
+    BDS = BDS_under + BDS_resp
+    if zsnow > zmax:
+        zsnow = zmax
+    diff: np.ndarray = Csat_table[zcc : int(zsnow)] - co3
+    area_p: np.ndarray = area_dz_table[zcc : int(zsnow)]
+    # integrate saturation difference over area
+    BPDC = kc * area_p.dot(diff)
+    BPDC = max(BPDC, 0)  # prevent negative values
+    d_zsnow = -BPDC / (area_dz_table[int(zsnow)] * I_caco3)
+    dMdt_DIC: float = BDS + BCC + BNS + BPDC
+
+    """CACO3_export is the flux of CaCO3 into the box. However, the model should
+    use the bypass option and leave all flux calculations to the
+    cs_code.  As such, we simply add the fraction of the input flux
+    that dissolves, and ignore the fraction that is buried.  
+
+    The isotope ratio of the dissolution flux is determined by the delta
+    value of the sediments we are dissolving, and the delta of the carbonate rain.
+    The currrent code, assumes that both are the same.
+    """
+    dMdt_CaCO3_l = dMdt_DIC * dic_sb_l / dic_sb
+    dH = hplus - hplus_0
+
+    # F_DIC, F_DIC_l, F_TA, dH, d_zsnow
+    return -dMdt_DIC, -dMdt_CaCO3_l, -2 * dMdt_DIC, dH, d_zsnow
+
+
+def init_carbonate_system_3(
+    rg: ReservoirGroup,
+    export_flux: Flux,
+    r_sb: ReservoirGroup,
+    r_db: ReservoirGroup,
+    area_table: np.ndarray,
+    area_dz_table: np.ndarray,
+    Csat_table: np.ndarray,
+    AD: float,
+    kwargs: dict,
+):
+
+    from esbmtk import ExternalCode, carbonate_system_3
+
+    ec = ExternalCode(
+        name="cs",
+        species=rg.mo.Carbon.CO2,
+        function=carbonate_system_3,
+        fname="carbonate_system_3",
+        ftype="cs2",
+        r_s=r_sb,  # source (RG) of CaCO3 flux,
+        r_d=r_db,  # sink (RG) of CaCO3 flux,
+        vr_datafields={
+            "depth_area_table": area_table,
+            "area_dz_table": area_dz_table,
+            "Csat_table": Csat_table,
+        },
+        function_input_data=[
+            rg,  # 0
+            export_flux,  # 1
+            r_db.DIC,  # 2
+            r_db.TA,  # 3
+            r_sb.DIC,  # 4
+            "Hplus",  # 5
+            "zsnow",  # 6
+            kwargs["Ksp0"],  # 7
+            float(kwargs["kc"]),  # 8
+            float(AD),  # 9
+            float(abs(kwargs["zsat0"])),  # 10
+            float(kwargs["I_caco3"]),  # 11
+            float(kwargs["alpha"]),  # 12
+            float(abs(kwargs["zsat_min"])),  # 13
+            float(abs(kwargs["zmax"])),  # 14
+            float(abs(kwargs["z0"])),  # 15
+        ],
+        return_values=[
+            {"F_rg.DIC": "db_remineralization"},
+            {"F_rg.TA": "db_remineralization"},
+            {"Hplus": rg.swc.hplus},
+            {"zsnow": float(abs(kwargs["zsnow"]))},
+        ],
+        register=rg,
+    )
+    rg.mo.lpc_f.append(ec.fname)
+
+    return ec
+
+
+def add_carbonate_system_3(**kwargs) -> None:
+    """Creates a new carbonate system virtual reservoir
+    which will compute carbon species, saturation, compensation,
+    and snowline depth, and compute the associated carbonate burial fluxes
+
+    Required keywords:
+        r_sb: list of ReservoirGroup objects in the surface layer
+        r_db: list of ReservoirGroup objects in the deep layer
+        carbonate_export_fluxes: list of flux objects which must match the
+                                 list of ReservoirGroup objects.
+        zsat_min = depth of the upper boundary of the deep box
+        z0 = upper depth limit for carbonate burial calculations
+             typically zsat_min
+
+    Optional Parameters:
+
+        zsat = initial saturation depth (m)
+        zcc = initial carbon compensation depth (m)
+        zsnow = initial snowline depth (m)
+        zsat0 = characteristic depth (m)
+        Ksp0 = solubility product of calcite at air-water interface (mol^2/kg^2)
+        kc = heterogeneous rate constant/mass transfer coefficient for calcite dissolution (kg m^-2 yr^-1)
+        Ca2 = calcium ion concentration (mol/kg)
+        pc = characteristic pressure (atm)
+        pg = seawater density multiplied by gravity due to acceleration (atm/m)
+        I = dissolvable CaCO3 inventory
+        co3 = CO3 concentration (mol/kg)
+        Ksp = solubility product of calcite at in situ sea water conditions (mol^2/kg^2)
+
+    """
+
+    from esbmtk import Reservoir, init_carbonate_system_3
+
+    # list of known keywords
+    lkk: dict = {
+        "r_db": list,  # list of deep reservoirs
+        "r_sb": list,  # list of corresponding surface reservoirs
+        "carbonate_export_fluxes": list,
+        "AD": float,
+        "zsat": int,
+        "zsat_min": int,
+        "zcc": int,
+        "zsnow": int,
+        "zsat0": int,
+        "Ksp0": float,
+        "kc": float,
+        "Ca2": float,
+        "pc": (float, int),
+        "pg": (float, int),
+        "I_caco3": (float, int),
+        "alpha": float,
+        "zmax": (float, int),
+        "z0": (float, int),
+        "Ksp": (float, int),
+        # "BM": (float, int),
+    }
+    # provide a list of absolutely required keywords
+    lrk: list[str] = [
+        "r_db",
+        "r_sb",
+        "carbonate_export_fluxes",
+        "zsat_min",
+        "z0",
+    ]
+
+    # we need the reference to the Model in order to set some
+    # default values.
+
+    reservoir = kwargs["r_db"][0]
+    model = reservoir.mo
+    # list of default values if none provided
+    lod: dict = {
+        "r_sb": [],  # empty list
+        "zsat": -3715,  # m
+        "zcc": -4750,  # m
+        "zsnow": -5000,  # m
+        "zsat0": -5078,  # m
+        "Ksp0": reservoir.swc.Ksp0,  # mol^2/kg^2
+        "kc": 8.84 * 1000,  # m/yr converted to kg/(m^2 yr)
+        "AD": model.hyp.area_dz(-200, -6000),
+        "alpha": 0.6,  # 0.928771302395292, #0.75,
+        "pg": 0.103,  # pressure in atm/m
+        "pc": 511,  # characteristic pressure after Boudreau 2010
+        "I_caco3": 529,  # dissolveable CaCO3 in mol/m^2
+        "zmax": -6000,  # max model depth
+        "Ksp": reservoir.swc.Ksp,  # mol^2/kg^2
+    }
+
+    # make sure all mandatory keywords are present
+    __checkkeys__(lrk, lkk, kwargs)
+    # add default values for keys which were not specified
+    kwargs = __addmissingdefaults__(lod, kwargs)
+    # test that all keyword values are of the correct type
+    __checktypes__(lkk, kwargs)
+
+    # establish some shared parameters
+    # depths_table = np.arange(0, 6001, 1)
+    depths: np.ndarray = np.arange(0, 6002, 1, dtype=float)
+    r_db = kwargs["r_db"]
+    r_sb = kwargs["r_sb"]
+    ca2 = r_db[0].swc.ca2
+    pg = kwargs["pg"]
+    pc = kwargs["pc"]
+    z0 = kwargs["z0"]
+    Ksp0 = kwargs["Ksp0"]
+    # C saturation(z) after Boudreau 2010
+    Csat_table: np.ndarray = (Ksp0 / ca2) * np.exp((depths * pg) / pc)
+    area_table = model.hyp.get_lookup_table(0, -6002)  # area in m^2(z)
+    area_dz_table = model.hyp.get_lookup_table_area_dz(0, -6002) * -1  # area'
+    AD = model.hyp.area_dz(z0, -6000)  # Total Ocean Area
+
+    for i, rg in enumerate(r_db):  # Setup the virtual reservoirs
+
+        ec = init_carbonate_system_3(
+            rg,
+            kwargs["carbonate_export_fluxes"][i],
+            r_sb[i],
+            r_db[i],
+            area_table,
+            area_dz_table,
+            Csat_table,
+            AD,
+            kwargs,
+        )
+
+        register_return_values(ec, rg)
+        rg.has_cs2 = True
