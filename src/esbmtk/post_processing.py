d807ddc2c53dc24cb62377259f5bcc4f113d3fa1
uliw <uli.wortmann@utoronto.ca>
2023-12-03 10:13:50 -0500

    feat(post_processing): carbonate_system_1_pp and
    carbonate_system_2_pp functions now accept lists as input
    This is untested

--- src/esbmtk/post_processing.py
+++ src/esbmtk/post_processing.py
@@ -10,7 +10,7 @@ if tp.TYPE_CHECKING:
 NDArrayFloat = npt.NDArray[np.float64]
 
 
-def carbonate_system_1_pp(rg: ReservoirGroup) -> None:
+def carbonate_system_1_pp(bn: ReservoirGroup) -> None:
     """Calculates and returns various carbonate species based on previously calculated
     Hplus, TA, and DIC concentrations.
 
@@ -27,45 +27,47 @@ def carbonate_system_1_pp(rg: ReservoirGroup) -> None:
     """
     from esbmtk import VectorData
 
-    k1 = rg.swc.K1  # K1
-    k2 = rg.swc.K2  # K2
-    k1k2 = rg.swc.K1K2
-    hplus = rg.Hplus.c
-    dic = rg.DIC.c
+    if not isinstance(bn, list):
+        bn = [bn]
 
-    VectorData(
-        name="CO3",
-        register=rg,
-        species=rg.mo.CO3,
-        data=dic / (1 + hplus / k2 + hplus**2 / k1k2),
-        label="CO32-",
-        plt_units=rg.mo.c_unit,
-    )
+    for rg in bn:
+        k1 = rg.swc.K1  # K1
+        k2 = rg.swc.K2  # K2
+        k1k2 = rg.swc.K1K2
+        hplus = rg.Hplus.c
+        dic = rg.DIC.c
 
-    VectorData(
-        name="pH",
-        register=rg,
-        species=rg.mo.pH,
-        data=-np.log10(hplus),
-        label="pH",
-        plt_units="total scale",
-    )
+        VectorData(
+            name="CO3",
+            register=rg,
+            species=rg.mo.CO3,
+            data=dic / (1 + hplus / k2 + hplus**2 / k1k2),
+            label="CO32-",
+            plt_units=rg.mo.c_unit,
+        )
 
-    VectorData(
-        name="Omega",
-        register=rg,
-        species=rg.mo.pH,
-        data=rg.swc.ca2 * rg.CO3.c / rg.swc.Ksp_ca,
-        label=r"$\Omega$-Calcite",
-        plt_units="",
-    )
-    # rg.omega = rg.swc.ca2 * rg.CO3 / rg.swc.Ksp
-    # rg.CO2aq: float = dic / (1 + (k1 / hplus) + (k1 * k2 / (hplus**2)))
+        VectorData(
+            name="pH",
+            register=rg,
+            species=rg.mo.pH,
+            data=-np.log10(hplus),
+            label="pH",
+            plt_units="total scale",
+        )
 
+        VectorData(
+            name="Omega",
+            register=rg,
+            species=rg.mo.pH,
+            data=rg.swc.ca2 * rg.CO3.c / rg.swc.Ksp_ca,
+            label=r"$\Omega$-Calcite",
+            plt_units="",
+        )
+    
 
 def carbonate_system_2_pp(
-    rg: ReservoirGroup,  # 2 Reservoir handle
-    export: float,  # 3 CaCO3 export flux as DIC
+    bn: ReservoirGroup,  # 2 Reservoir handle
+    export_fluxes: float,  # 3 CaCO3 export flux as DIC
     zsat_min: float,
     zmax: float,
 ) -> None:
@@ -99,112 +101,119 @@ def carbonate_system_2_pp(
 
     from esbmtk import VectorData
 
-    p = rg.cs.function_params
-    sp, cp, area_table, area_dz_table, Csat_table = p
-    ksp0, kc, AD, zsat0, I_caco3, alpha, zsat_min, zmax, z0 = cp
-    k1, k2, k1k2, KW, KB, ca2, boron, isotopes = sp
-    hplus = rg.Hplus.c
-    dic = rg.DIC.c
-    zsnow = rg.zsnow.c.astype(int)
-    area_table = rg.model.area_table
-    area_dz_table = rg.model.area_dz_table
-    Csat_table = rg.model.Csat_table
+    if not isinstance(bn, list):
+        bn = [bn]
+    if not isinstance(export_fluxes, list):
+        export_fluxes = [export_fluxes]
+        
+    for i, rg in enumerate(bn):
+        export = export_fluxes[i].c
+        p = rg.cs.function_params
+        sp, cp, area_table, area_dz_table, Csat_table = p
+        ksp0, kc, AD, zsat0, I_caco3, alpha, zsat_min, zmax, z0 = cp
+        k1, k2, k1k2, KW, KB, ca2, boron, isotopes = sp
+        hplus = rg.Hplus.c
+        dic = rg.DIC.c
+        zsnow = rg.zsnow.c.astype(int)
+        area_table = rg.model.area_table
+        area_dz_table = rg.model.area_dz_table
+        Csat_table = rg.model.Csat_table
 
-    # hco3 = dic / (1 + hplus / k1 + k2 / hplus)
-    co3 = dic / (1 + hplus / k2 + hplus**2 / k1k2)
-    co2aq = dic / (1 + k1 / hplus + k1k2 / hplus**2)
-    zsat = np.clip(zsat0 * np.log(ca2 * co3 / ksp0), zsat_min, zmax).astype(int)
-    zcc = (zsat0 * np.log(export * ca2 / (ksp0 * AD * kc) + ca2 * co3 / ksp0)).astype(
-        int
-    )
-    B_AD = export / AD
+        # hco3 = dic / (1 + hplus / k1 + k2 / hplus)
+        co3 = dic / (1 + hplus / k2 + hplus**2 / k1k2)
+        co2aq = dic / (1 + k1 / hplus + k1k2 / hplus**2)
+        zsat = np.clip(zsat0 * np.log(ca2 * co3 / ksp0), zsat_min, zmax).astype(int)
+        zcc = (zsat0 * np.log(export * ca2 / (ksp0 * AD * kc) + ca2 * co3 / ksp0)).astype(
+            int
+        )
+        B_AD = export / AD
 
-    A_z0_zsat = area_table[z0] - area_table[zsat]
-    A_zsat_zcc = area_table[zsat] - area_table[zcc]
-    A_zcc_zmax = area_table[zcc] - area_table[zmax]
+        A_z0_zsat = area_table[z0] - area_table[zsat]
+        A_zsat_zcc = area_table[zsat] - area_table[zcc]
+        A_zcc_zmax = area_table[zcc] - area_table[zmax]
 
-    # must be loop
-    Fdiss = zsat * 0
-    Fburial = zsat * 0
+        # must be loop
+        Fdiss = zsat * 0
+        Fburial = zsat * 0
 
-    for i, e in enumerate(zsat):
-        BCC = A_zcc_zmax[i] * B_AD
-        BNS = alpha * A_z0_zsat[i] * B_AD
-        diff_co3 = Csat_table[zsat[i] : zcc[i]] - co3[i]
-        area_p = area_dz_table[zsat[i] : zcc[i]]
-        BDS_under = kc * area_p.dot(diff_co3)
-        BDS_resp = alpha * (A_zsat_zcc[i] * B_AD - BDS_under)
-        BDS = BDS_under + BDS_resp
-        if zsnow[i] > zmax:
-            zsnow[i] = zmax
-        # integrate satu ration difference over area
-        diff = Csat_table[zcc[i] : zsnow[i]] - co3[i]
-        area_p = area_dz_table[zcc[i] : zsnow[i]]
-        BPDC = kc * area_p.dot(diff)
-        BPDC = max(BPDC, 0)  # prevent negative values
-        Fdiss[i] = BDS + BCC + BNS + BPDC
-        Fburial[i] = export - Fdiss[i]
+        for i, e in enumerate(zsat):
+            BCC = A_zcc_zmax[i] * B_AD
+            BNS = alpha * A_z0_zsat[i] * B_AD
+            diff_co3 = Csat_table[zsat[i] : zcc[i]] - co3[i]
+            area_p = area_dz_table[zsat[i] : zcc[i]]
+            BDS_under = kc * area_p.dot(diff_co3)
+            BDS_resp = alpha * (A_zsat_zcc[i] * B_AD - BDS_under)
+            BDS = BDS_under + BDS_resp
+            if zsnow[i] > zmax:
+                zsnow[i] = zmax
+            # integrate satu ration difference over area
+            diff = Csat_table[zcc[i] : zsnow[i]] - co3[i]
+            area_p = area_dz_table[zcc[i] : zsnow[i]]
+            BPDC = kc * area_p.dot(diff)
+            BPDC = max(BPDC, 0)  # prevent negative values
+            Fdiss[i] = BDS + BCC + BNS + BPDC
+            Fburial[i] = export - Fdiss[i]
 
-    VectorData(
-        name="Fburial",
-        register=rg,
-        species=rg.mo.Fburial,
-        data=Fburial,
-        label="Fburial",
-        plt_units=rg.mo.f_unit,
-    )
+        VectorData(
+            name="Fburial",
+            register=rg,
+            species=rg.mo.Fburial,
+            data=Fburial,
+            label="Fburial",
+            plt_units=rg.mo.f_unit,
+        )
 
-    VectorData(
-        name="Fdiss",
-        register=rg,
-        species=rg.mo.Fdiss,
-        data=Fdiss,
-        label="Fdiss",
-        plt_units=rg.mo.f_unit,
-    )
+        VectorData(
+            name="Fdiss",
+            register=rg,
+            species=rg.mo.Fdiss,
+            data=Fdiss,
+            label="Fdiss",
+            plt_units=rg.mo.f_unit,
+        )
 
-    VectorData(
-        name="CO3",
-        register=rg,
-        species=rg.mo.CO3,
-        data=co3,
-        label="CO32-",
-        plt_units=rg.mo.c_unit,
-    )
+        VectorData(
+            name="CO3",
+            register=rg,
+            species=rg.mo.CO3,
+            data=co3,
+            label="CO32-",
+            plt_units=rg.mo.c_unit,
+        )
 
-    VectorData(
-        name="CO2aq",
-        register=rg,
-        species=rg.mo.CO2aq,
-        data=co2aq,
-        label="CO2aq",
-        plt_units=rg.mo.c_unit,
-    )
+        VectorData(
+            name="CO2aq",
+            register=rg,
+            species=rg.mo.CO2aq,
+            data=co2aq,
+            label="CO2aq",
+            plt_units=rg.mo.c_unit,
+        )
 
-    VectorData(
-        name="pH",
-        register=rg,
-        species=rg.mo.pH,
-        data=-np.log10(hplus),
-        label="pH",
-        plt_units="total scale",
-    )
-    VectorData(
-        name="zsat",
-        register=rg,
-        species=rg.mo.zsat,
-        data=zsat,
-        label="zsat",
-        plt_units="m",
-    )
-    VectorData(
-        name="zcc",
-        register=rg,
-        species=rg.mo.zcc,
-        data=zcc,
-        label="zcc",
-        plt_units="m",
-    )
+        VectorData(
+            name="pH",
+            register=rg,
+            species=rg.mo.pH,
+            data=-np.log10(hplus),
+            label="pH",
+            plt_units="total scale",
+        )
+        VectorData(
+            name="zsat",
+            register=rg,
+            species=rg.mo.zsat,
+            data=zsat,
+            label="zsat",
+            plt_units="m",
+        )
+        VectorData(
+            name="zcc",
+            register=rg,
+            species=rg.mo.zcc,
+            data=zcc,
+            label="zcc",
+            plt_units="m",
+        )
 
 
 def gas_exchange_fluxes(
