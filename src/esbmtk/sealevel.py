72a8e14d317ca6d9380d2e0553a028b172a69047
zhongjingwen <lisazjw.zhong@mail.utoronto.ca>
2024-05-30 14:29:50 -0400

    add functions of hypsometry

--- src/esbmtk/sealevel.py
+++ src/esbmtk/sealevel.py
@@ -23,9 +23,9 @@
 
 import numpy as np
 import numpy.typing as npt
-import matplotlib.pyplot as plt
 import pandas as pd
 from .esbmtk_base import esbmtkBase
+from math import cos, sin, radians, pi
 
 # declare numpy types
 NDArrayFloat = npt.NDArray[np.float64]
@@ -352,3 +352,133 @@ def get_box_geometry_parameters(box, fraction=1) -> None:
     # box.area_fraction = box.sed_area / box.reference_area
     box.area_dz = box.sed_area
     box.geometry_unset = False
+
+
+def earth_radius(
+    lat: float,
+) -> float:
+    """Get earth radius as function of latitude
+
+    :param lat: latitude in degrees
+    :type lat: float
+    :return: radius of earth in meters
+    :rtype: float
+    """
+    a = 6378137  # equatorial radius
+    b = 6356752  # polar radius
+
+    lat = radians(lat)
+    r = (
+        ((a**2 * cos(lat)) ** 2 + (b**2 * sin(lat)) ** 2)
+        / ((a * cos(lat)) ** 2 + (b * sin(lat)) ** 2)
+    ) ** 0.5
+    return r
+
+
+def grid_area(
+    lat: float,
+    size: float,
+) -> float:
+    """Calculate the area of a rectangular area of size = 1 deg at a given
+        lat-long position.
+
+    :param lat: latitude in degrees
+    :type lat: float
+    :param size: size of the rectangular area in degrees
+    :type size: float
+    :return: area in square meters
+    :rtype: float
+    """
+    r = earth_radius(lat) / 1000
+    dy = (size * r * pi) / 180
+    dx = size / 180 * pi * r * cos(radians(lat))
+    return abs(dx * dy)
+
+
+def slice_count(
+    start: int,
+    end: int,
+    weight: NDArrayFloat,
+    grid: NDArrayFloat,
+    elevation_minimum: float,
+    elevation_maximum: float,
+    elevations: NDArrayFloat,
+    dz: int,
+) -> NDArrayFloat:
+    """Generate elevation count array for each latitudinal slice which
+        summarized the count of elevation values in each elevation
+        interval in current slice.
+
+    :param start: start index of the slice
+    :type start: int
+    :param end: end index of the slice
+    :type end: int
+    :param weight: weight array for each latitudinal slice
+    :type weight: NDArrayFloat
+    :param grid: grid of all the data about to be sliced
+    :type grid: NDArrayFloat
+    :param elevation_minimum: minimum elevation
+    :type elevation_minimum: int
+    :param elevation_maximum: maximum elevation
+    :type elevation_maximum: int
+    :param elevations: elevation array
+    :type elevations: NDArrayFloat
+    :param dz: elevation interval
+    :type dz: int
+    :return: elevation count array for each latitudinal slice
+    :rtype: NDArrayFloat
+    """
+    sub_grid = grid[start:end, ...]
+
+    count = np.zeros(int((elevation_maximum - elevation_minimum) // dz),
+                     dtype=float)
+
+    for i, e in enumerate(elevations):
+        a = np.sum(np.logical_and(sub_grid > e, sub_grid < e + dz), axis=1)
+        count[i] = np.sum(a * weight)
+
+    return count
+
+
+def process_slice(
+    start: int,
+    end: int,
+    lat: NDArrayFloat,
+    grid: NDArrayFloat,
+    dz: int,
+    elevation_minimum: float,
+    elevation_maximum: float,
+    elevations: NDArrayFloat,
+    dx: float,
+) -> NDArrayFloat:
+    """Take grid area in to account when calculating the elevation count,
+        as earth is elliptical, the grid area for same latitude and longitude
+        gap is different, the function adjust the weight for each slice and
+        return the weighted elevation count array for each slice.
+
+    :param start: start index of the slice
+    :type start: int
+    :param end: end index of the slice
+    :type end: int
+    :param lat: latitude array
+    :type lat: NDArrayFloat
+    :param grid: grid of elevation data
+    :type grid: NDArrayFloat
+    :param dz: elevation interval
+    :type dz: int
+    :param elevation_minimum: minimum elevation in the grid
+    :type elevation_minimum: int
+    :param elevation_maximum: maximum elevation in the grid
+    :type elevation_maximum: int
+    :param elevations: elevation array
+    :type elevations: NDArrayFloat
+    :param dx: grid resolution in degrees
+    :type dx: float
+    :return: elevation count array for each latitudinal slice
+    :rtype: NDArrayFloat
+    """
+    lat_slice = lat[start:end]
+    weight = np.array([grid_area(lat_val, dx) for lat_val in lat_slice])
+    return slice_count(
+        start, end, weight, grid, elevation_minimum, elevation_maximum,
+        elevations, dz)
