6753d998fc2c9002cdba02c60f67e391bcede4c9
Magnus Roland Marun <magnus.roland.marun@gmail.com>
2024-06-27 12:23:04 -0400

    restructured experiment

--- src/esbmtk/experiments.py
+++ src/esbmtk/experiments.py
@@ -16,19 +16,18 @@
      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import esbmtk
-
 from esbmtk import (
     Model,
-    Reservoir,
+    # Reservoir,
     ConnectionProperties,
     SourceProperties,
     SinkProperties,
+    ExternalCode,
     Q_,
 )
 
 
-def calculate_burial(po4_export_flux: float, o2_con: float) -> float:
+def calculate_burial(po4_export_flux: float, o2_c: float) -> float:
     """#add an empty tuple
     Calculate burial as a function of productivity and oxygen concentration.
 
@@ -39,30 +38,26 @@ def calculate_burial(po4_export_flux: float, o2_con: float) -> float:
     :return: Burial flux in mol/year
     :rtype: float
     """
-    # burial fraction to [oxygen] approximation of relationship from 0.01 to 0.1
+
     min_burial_fraction = 0.01
     max_burial_fraction = 0.1
-    burial_fraction = min_burial_fraction + (
-        max_burial_fraction - min_burial_fraction
-    ) * (o2_con / 100)
+    F_burial = min_burial_fraction + (max_burial_fraction - min_burial_fraction) * (
+        o2_c / 100
+    )
 
-    deep_ocean_v = 1e18  # in litres
+    dbv = 1e18  # L
 
     # productivity in mol/year
-    productivity_mol_year = (
-        po4_export_flux * deep_ocean_v * 1e-6
-    )  # Convert umol/L to mol
+    productivity_mol_year = po4_export_flux * dbv * 1e-6  # Convert umol/L to mol
 
-    burial_flux = productivity_mol_year * burial_fraction
+    burial_flux = productivity_mol_year * F_burial
+    p = F_burial, dbv
 
     return burial_flux
 
 
-from esbmtk import ExternalCode
-
-
 def add_my_burial(
-    source, sink, species, po4_export_flux: float, o2_con: float, scale
+    source, sink, species, o2_c, po4_export_flux: float, F_burial, dbv
 ) -> None:
     """This function initializes a user supplied function
     so that it can be used within the ESBMTK ecosystem.
@@ -79,16 +74,16 @@ def add_my_burial(
         PO4 export flux in umol/L
     o2_con : float
         Oxygen concentration in umol/L
-    scale : float
-        A scaling factor
+    F_b : float
+        A scaling factor of burial fraction
     """
-    p = (scale,)  # float into tuple
+    p = (F_burial, dbv)  # float into tuple
     ec = ExternalCode(
         name="calculate_burial",
         species=species,
         function=calculate_burial,
         fname="calculate_burial",
-        function_input_data=[po4_export_flux, F_b],
+        function_input_data=[po4_export_flux, o2_c],
         function_params=p,
         return_values=[
             {f"F_{sink}.{M.PO4}": "po4_burial"},
