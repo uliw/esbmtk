c6be996a53cac7049e895e89065a68e10bd426a7
uliw <uli.wortmann@utoronto.ca>
2024-12-18 13:41:08 -0500

    feat: Update test result values in manual pages and
    corresponding test scripts

--- tests/test_po4_2.py
+++ tests/test_po4_2.py
@@ -3,8 +3,8 @@ import po4_2_test  # import script
 
 M = po4_2_test.M  # get model handle 
 test_values = [ # result, reference value
-    (M.S_b.PO4.c[-1], 3.3378336265779095e-05),
-    (M.D_b.PO4.c[-1], 4.9100854586867445e-05),
+    (M.S_b.PO4.c[-1], 3.318233001125679e-05),
+    (M.D_b.PO4.c[-1], 4.881170094975448e-05),
 ]
 # run tests
 @pytest.mark.parametrize("test_input, expected", test_values)
