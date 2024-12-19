7712f8351dfa42c985b9e66af293fd05973052fd
Irene <i.ma@mail.utoronto.ca>
2024-12-19 11:48:23 -0500

    Changing print statements

--- src/esbmtk/extended_classes.py
+++ src/esbmtk/extended_classes.py
@@ -645,8 +645,8 @@ class Signal(esbmtkBase):
         mask = np.in1d(model_time, signal_time)
         mapped_time[mask] = model_time[mask]
         print(f"Model time: {model_time}")
-        print(signal_time)
-        print(len(signal_time))
+        print(f"Signal time: {signal_time}")
+        print(f"Signal time length: {len(signal_time)})
         print(len(self.s_m))
         print(len(mapped_time))
 
