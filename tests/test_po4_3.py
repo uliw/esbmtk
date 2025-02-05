30431f7051b205663096eb55116bf72ead53e8f9
uliw <uli.wortmann@utoronto.ca>
2025-02-05 15:34:06 -0500

    fix: Correct author's name capitalization in CHANGELOG.rst and
    source.rst files, updated test parameters

--- tests/test_po4_3.py
+++ tests/test_po4_3.py
@@ -3,7 +3,7 @@ import po4_3_test  # import script
 
 M = po4_3_test.M  # get model handle 
 test_values = [ # result, reference value
-    (M.S_b.PO4.c[-1]*1e6, 1.49),
+    (M.S_b.PO4.c[-1]*1e6, 14.99),
     (M.S_b.DIC.c[-1]*1e6, 1589.98),
     (M.D_b.PO4.c[-1]*1e6, 22.05),
     (M.D_b.DIC.c[-1]*1e6, 2338.18),
