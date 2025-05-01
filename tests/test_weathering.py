6647155df841b07b65a47c046533dc67b81b5360
uliw <uli.wortmann@utoronto.ca>
2025-05-01 16:25:15 -0400

    test: expand test cases for weathering flux with isotopes
    Added new test cases to cover scenarios with varying pCO2 levels and isotope effects. Updated box parameters and introduced additional gas reservoirs to support these cases. Adjusted test assertions to validate new conditions, including delta and epsilon variations.

--- tests/test_weathering.py
+++ tests/test_weathering.py
@@ -22,7 +22,7 @@ M = Model(
 )
 
 box_parameters: dict = {
-    "b1": {  # empty box, no isotopes
+    "b0": {
         "c": {
             M.DIC: "0 umol/kg",
             M.TA: "0 umol/kg",
@@ -34,7 +34,7 @@ box_parameters: dict = {
         "S": 35,
         "d": {M.DIC: 0},
     },
-    "b2": {  # empty box, no isotopes
+    "b1": {
         "c": {
             M.DIC: "0 umol/kg",
             M.TA: "0 umol/kg",
@@ -46,7 +46,55 @@ box_parameters: dict = {
         "S": 35,
         "d": {M.DIC: 0},
     },
-    "b3": {  # empty box, no isotopes
+    "b2": {
+        "c": {
+            M.DIC: "0 umol/kg",
+            M.TA: "0 umol/kg",
+            M.O2: "0 umol/kg",
+        },
+        "g": [0, -10000, 1],
+        "T": 2,
+        "P": 240,
+        "S": 35,
+        "d": {M.DIC: 0},
+    },
+    "b3": {
+        "c": {
+            M.DIC: "0 umol/kg",
+            M.TA: "0 umol/kg",
+            M.O2: "0 umol/kg",
+        },
+        "g": [0, -10000, 1],
+        "T": 2,
+        "P": 240,
+        "S": 35,
+        "d": {M.DIC: 0},
+    },
+    "b4": {
+        "c": {
+            M.DIC: "0 umol/kg",
+            M.TA: "0 umol/kg",
+            M.O2: "0 umol/kg",
+        },
+        "g": [0, -10000, 1],
+        "T": 2,
+        "P": 240,
+        "S": 35,
+        "d": {M.DIC: 0},
+    },
+    "b5": {
+        "c": {
+            M.DIC: "0 umol/kg",
+            M.TA: "0 umol/kg",
+            M.O2: "0 umol/kg",
+        },
+        "g": [0, -10000, 1],
+        "T": 2,
+        "P": 240,
+        "S": 35,
+        "d": {M.DIC: 0},
+    },
+    "b6": {
         "c": {
             M.DIC: "0 umol/kg",
             M.TA: "0 umol/kg",
@@ -72,13 +120,21 @@ GasReservoir(
     species_ppm="280 ppm",
     delta=-7,
 )
+
+GasReservoir(
+    name="CO2_At_with_isotopes_240",
+    species=M.CO2,
+    species_ppm="240 ppm",
+    delta=-7,
+)
+
 GasReservoir(
     name="CO2_At_no_isotopes",
     species=M.CO2,
     species_ppm="280 ppm",
 )
 
-# ---- Test regular weathering flux ---- #
+# ---- Test regular weathering flux O2 only ---- #
 Species2Species(  # CaCO3 weathering as function of carbonate weathering
     ctype="weathering",
     source=M.Fw.O2,  # source of flux
@@ -103,6 +159,20 @@ Species2Species(  # CaCO3 weathering as function of carbonate weathering
     rate="10 Tmol/yr",  # rate at pco2_0
     id="weathering_yes_no",
 )
+
+# ---- Test where source and sink have isotopes
+# # case 0, no isotope effects
+# Species2Species(  # CaCO3 weathering as function of carbonate weathering
+#     ctype="weathering",
+#     source=M.Fw.DIC,  # source of flux
+#     sink=M.b0.DIC,
+#     reservoir_ref=M.CO2_At_with_isotopes,  # pCO2
+#     scale=1,  # optional, defaults to 1
+#     ex=0.4,  # exponent c
+#     pco2_0="280 ppm",  # reference pCO2
+#     rate="10 Tmol/yr",  # rate at pco2_0
+#     id="weathering_yes_no",
+# )
 
 # ---- Test regular weathering flux where atmosphere and sink have isotopes ---- #
 # case one: no isotope effects
@@ -132,7 +202,7 @@ Species2Species(  # CaCO3 weathering as function of carbonate weathering
     delta=5,
 )
 
-# case three: constant delta
+# case three: constant epsilon
 Species2Species(  # CaCO3 weathering as function of carbonate weathering
     ctype="weathering",
     source=M.Fw.DIC,  # source of flux
@@ -146,29 +216,80 @@ Species2Species(  # CaCO3 weathering as function of carbonate weathering
     epsilon=21,
 )
 
+# case 4: constant delta but different pCO2
+Species2Species(  # CaCO3 weathering as function of carbonate weathering
+    ctype="weathering",
+    source=M.Fw.DIC,  # source of flux
+    sink=M.b4.DIC,
+    reservoir_ref=M.CO2_At_with_isotopes_240,  # pCO2
+    scale=1,  # optional, defaults to 1
+    ex=0.4,  # exponent c
+    pco2_0="280 ppm",  # reference pCO2
+    rate="10 Tmol/yr",  # rate at pco2_0
+    id="weathering_yes_yes_4_240",
+    delta=5,
+)
+
+# case 5: epsilon  but different pCO2
+Species2Species(  # CaCO3 weathering as function of carbonate weathering
+    ctype="weathering",
+    source=M.Fw.DIC,  # source of flux
+    sink=M.b5.DIC,
+    reservoir_ref=M.CO2_At_with_isotopes_240,  # pCO2
+    scale=1,  # optional, defaults to 1
+    ex=0.4,  # exponent c
+    pco2_0="280 ppm",  # reference pCO2
+    rate="10 Tmol/yr",  # rate at pco2_0
+    id="weathering_yes_yes_5_240",
+    epsilon=21,
+)
+
+# case 6: isotopes, no effect but different pCO2
+Species2Species(  # CaCO3 weathering as function of carbonate weathering
+    ctype="weathering",
+    source=M.Fw.DIC,  # source of flux
+    sink=M.b6.DIC,
+    reservoir_ref=M.CO2_At_with_isotopes_240,  # pCO2
+    scale=1,  # optional, defaults to 1
+    ex=0.4,  # exponent c
+    pco2_0="280 ppm",  # reference pCO2
+    rate="10 Tmol/yr",  # rate at pco2_0
+    id="weathering_yes_yes_6_240",
+)
+
+
 # M.debug_equations_file = True
 M.run()
 
 # regular weathering flux
 wnn_mass = M.b1.O2.volume * M.b1.swc.density / 1000 * M.b1.O2.c[-1]
 wyn_mass = M.b2.O2.volume * M.b1.swc.density / 1000 * M.b1.O2.c[-1]
+wyy0_mass = M.b1.DIC.volume * M.b1.swc.density / 1000 * M.b1.DIC.c[-1]
 wyy1_mass = M.b1.DIC.volume * M.b1.swc.density / 1000 * M.b1.DIC.c[-1]
 wyy2_mass = M.b2.DIC.volume * M.b2.swc.density / 1000 * M.b2.DIC.c[-1]
 wyy3_mass = M.b3.DIC.volume * M.b3.swc.density / 1000 * M.b3.DIC.c[-1]
+# wyy_d0 = M.b0.DIC.d[-1]
 wyy_d1 = M.b1.DIC.d[-1]
 wyy_d2 = M.b2.DIC.d[-1]
 wyy_d3 = M.b3.DIC.d[-1]
+wyy_d4 = M.b4.DIC.d[-1]
+wyy_d5 = M.b5.DIC.d[-1]
+wyy_d6 = M.b6.DIC.d[-1]
 
 tolerance = 1e-5
 test_values = [  # result, reference value, tolerance
     (wnn_mass.magnitude, 1e16, tolerance, "mass isotopes no no"),
     (wyn_mass.magnitude, 1e16, tolerance, "mass isotopes yes no"),
+    #    (wyy0_mass.magnitude, 1e16, tolerance, "mass isotopes yes yes 0"),
     (wyy1_mass.magnitude, 1e16, tolerance, "mass isotopes yes yes 1"),
     (wyy2_mass.magnitude, 1e16, tolerance, "mass isotopes yes yes 2"),
     (wyy3_mass.magnitude, 1e16, tolerance, "mass isotopes yes yes 3"),
-    (wyy_d1, 0, tolerance, "delta isotopes yes yes 1"),
+    (wyy_d1, 0, tolerance, "None isotopes yes yes 1"),
     (wyy_d2, 5, tolerance, "delta isotopes yes yes 2"),
-    (wyy_d3, 21, tolerance, "delta isotopes yes yes 2"),
+    (wyy_d3, 21, tolerance, "delta isotopes yes yes 3"),
+    (wyy_d4, 5, tolerance, "delta isotopes yes yes 4_240"),
+    (wyy_d5, 21, tolerance, "delta isotopes yes yes 5_240"),
+    (wyy_d6, 0, tolerance, "delta isotopes yes yes 6_240"),
 ]
 
 
