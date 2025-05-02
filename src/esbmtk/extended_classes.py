38359aba3017c320f04f5b401f1e5c05a571dd23
uliw <uli.wortmann@utoronto.ca>
2025-05-02 13:06:41 -0400

    refactor(extended_classes): add alias for data attribute in
    VectorData class

--- src/esbmtk/extended_classes.py
+++ src/esbmtk/extended_classes.py
@@ -1121,6 +1121,7 @@ class VectorData(esbmtkBase):
         self.mo = self.species.mo
         self.model = self.species.mo
         self.c = self.data
+        self.x = self.c
         self.label = self.name
         self.register.model.lvd.append(self)
         self.__register_with_parent__()
