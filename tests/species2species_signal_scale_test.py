aa1bc6918131a2b56d3799655d1770740d07626a
uliw <uli.wortmann@utoronto.ca>
2025-11-11 16:28:00 -0500

    test: add species2species signal scaling integration and validation tests
    - add species2species_signal_scale_test.py for signal scaling scenario
    - add test_species2species_signal_scaling.py to validate model output
    - ensure connection scaling factor is applied and tested against reference values

---
+++ tests/species2species_signal_scale_test.py
@@ -0,0 +1,60 @@
+"""Test signal scdaling.
+
+1. read signal from file
+2. connect signal to pre-existing connection
+3. modify connection scalingf factor after the fact.
+"""
+
+# import classes from the esbmtk library
+from esbmtk import (
+    Species2Species,  # the connection class
+    Model,  # the model class
+    Reservoir,  # the reservoir class
+    Signal,
+    SourceProperties,  # the source class
+)
+
+# define the basic model parameters
+M = Model(
+    stop="3 Myr",  # end time of model
+    max_timestep="1 kyr",  # upper limit of time step
+    element=["Carbon"],  # list of element definitions
+    debug_equations_file=False,
+)
+
+# boundary conditions
+F_w = M.set_flux("45 Gmol", "year", M.C)  # P @280 ppm (Filipelli 2002)
+# Source definitions
+SourceProperties(
+    name="weathering",
+    species=[M.DIC],
+)
+
+# reservoir definitions
+Reservoir(
+    name="S_b",  # box name
+    volume="3E16 m**3",  # surface box volume
+    concentration={M.DIC: "0 umol/l"},  # initial concentration
+)
+
+Signal(
+    name="CR",  # Signal name
+    species=M.DIC,  # SpeciesProperties
+    start="1 Myrs",
+    filename="signal_from_csv_data.csv",
+)
+
+Species2Species(
+    id="river",  # connection id
+    ctype="regular",
+    source=M.weathering.DIC,  # source of flux
+    sink=M.S_b.DIC,  # target of flux
+    rate=F_w,  # rate of flux
+    signal=M.CR,
+    scale=1,
+)
+
+M.Conn_weathering_to_S_b_DIC_river.scale = 5
+
+M.run()
+# M.plot([M.CR, M.S_b.DIC])
